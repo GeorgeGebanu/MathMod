63cc748856dd5c658c78406e989d2225a8281f53
Ольга Моисеева <olganaum13@yandex.ru>
2018-03-26 20:25:20 +0300

    допуск к зачету (курсовая полная)

--- Moiseeva.r
+++ Moiseeva.r
@@ -1,4 +1,4 @@
-library("tidyverse") 
+library("tidyverse")
 library("nycflights13") 
 library("tidyr") 
 library("stringr") 
@@ -8,8 +8,7 @@ library("readr")
 tbl=read.csv("eddypro.csv", skip = 1, na =c("","NA","-9999","-9999.0"), comment=c("[")) 
 tbl=tbl[-1,] 
 tbl 
-#избавляемся от NA
-tbl=tbl[tbl$DOY > 62 & tbl$DOY < 156,] 
+tbl=tbl[tbl$DOY > 62 & tbl$DOY < 156,]
 tbl
 #выбираем дни
 tbl=tbl[tbl$daytime == FALSE,] 
@@ -46,13 +45,36 @@ tbl_non_numeric = tbl[,!sapply(tbl,is.numeric) ]
 cor_td = cor(tbl_numeric)
 cor_td
 #корреляционный анализ не вышел, надо избавиться от строкгде есть NA
-cor_tbl = cor(drop_na(tbl_numeric)) 
-cor_tbl
-cor_tbl = cor(drop_na(tbl_numeric)) %>% as.data.frame %>% select(h2o_flux) 
-cor_tbl 
+cor_td = cor(drop_na(tbl_numeric)) 
+cor_td
+cor_td = cor(drop_na(tbl_numeric)) %>% as.data.frame %>% select(h2o_flux) 
+cor_td 
 vars = row.names(cor_tbl)[cor_tbl$h2o_flux^2 > .1] %>% na.exclude
 formula = as.formula(paste("h2o_flux~", paste(vars,collapse = "+"), sep="")) 
 #создаем формулу
-teaching_tbl = sample_n(tbl, floor(length(tbl$date)*.7)) 
-testing_tbl = sample_n(tbl, floor(length(tbl$date)*.3)) 
 formula
+mod=lm(formula, data = tbl) 
+anova(mod) 
+summary(mod) 
+formula1 = as.formula(paste("h2o_flux ~ Tau + rand_err_Tau + H + rand_err_H + LE + qc_LE + 
+                            rand_err_LE + co2_flux + h2o_flux + qc_h2o_flux + rand_err_h2o_flux + 
+                            co2_molar_density + co2_mole_fraction + co2_mixing_ratio + 
+                            h2o_time_lag + sonic_temperature + air_temperature + air_density + 
+                            air_molar_volume + es + RH + VPD + u_rot + wind_speed + max_speed + 
+                            u. + TKE + un_Tau + un_H + un_LE + un_co2_flux + un_h2o_flux + 
+                            u_var + v_var + w_var + h2o_var + w.ts_cov + w.co2_cov + 
+                            w.h2o_cov + co2 + co2.1 + flowrate")) 
+formula1 
+mod2=lm(formula1, data = tbl) 
+anova(mod2) 
+summary(mod2) 
+formula2 = as.formula(paste("h2o_flux ~ LE + qc_LE + RH + 
+                            VPD + max_speed + u. + un_LE + un_h2o_flux + h2o_var")) 
+mod3=lm(formula2, data = tbl) 
+anova(mod3) 
+summary(mod3) 
+formula3 = as.formula(paste("h2o_flux ~ LE + RH + 
+                            VPD + un_LE + un_h2o_flux + h2o_var")) 
+mod4=lm(formula3, data = tbl) 
+anova(mod4) 
+summary(mod4)
